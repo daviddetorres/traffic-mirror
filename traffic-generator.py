58231eeaae69b38154b7fb90b17b0a3515d470e9
David de Torres <david.detorres@sysdig.com>
2020-05-11 10:25:30 +0200

    Support for headers in locust script

--- traffic-generator.py
+++ traffic-generator.py
@@ -1,70 +1,91 @@
 from locust import Locust, TaskSet, task, between, HttpLocust
 
 class MyTaskSet(TaskSet):
-  @task(100)
-  def NoLatencyLightWeight(self):
-      self.client.get("/ok/0/500")
+    headers = {"Host": "traffic-mirror.io"}
 
-  @task(100)
-  def MediumLatencyLightWeight(self):
-      self.client.get("/ok/500/500")
+    @task(100)
+    def NoLatencyLightWeight(self):
+        self.client.request("GET","/ok/0/500", 
+                            headers=self.headers)
 
-  @task(10)
-  def HighLatencyLightWeight(self):
-      self.client.get("/ok/5000/500")
+    @task(100)
+    def MediumLatencyLightWeight(self):
+        self.client.request("GET","/ok/500/500", 
+                            headers=self.headers)
 
-  @task(100)
-  def NoLatencyMediumWeight(self):
-      self.client.get("/ok/0/50000")
+    @task(10)
+    def HighLatencyLightWeight(self):
+        self.client.request("GET","/ok/5000/500", 
+                            headers=self.headers)
+    
+    @task(100)
+    def NoLatencyMediumWeight(self):
+        self.client.request("GET","/ok/0/50000", 
+                            headers=self.headers)
+    
+    @task(100)
+    def MediumLatencyMediumWeight(self):
+        self.client.request("GET","/ok/500/50000", 
+                            headers=self.headers)
+    
+    @task(10)
+    def HighLatencyMediumWeight(self):
+        self.client.request("GET","/ok/5000/50000", 
+                            headers=self.headers)
 
-  @task(100)
-  def MediumLatencyMediumWeight(self):
-      self.client.get("/ok/500/50000")
+    @task(20)
+    def NoLatencyHeavyWeight(self):
+        self.client.request("GET","/ok/0/5000000", 
+                            headers=self.headers)
+    
+    @task(20)
+    def MediumLatencyHeavyWeight(self):
+        self.client.request("GET","/ok/500/5000000", 
+                            headers=self.headers)
+   
+    @task(10)
+    def HighLatencyHeavyWeight(self):
+        self.client.request("GET","/ok/5000/5000000", 
+                            headers=self.headers)
+    
+    @task(100)
+    def LightWeightPost(self):
+        self.client.request("POST","/ok/0/0",  
+                            data={"data":"0"*500}, 
+                            headers=self.headers)
+    
+    @task(100)
+    def MediumWeightPost(self):
+        self.client.request("POST","/ok/0/0", 
+                            data={"data":"0"*50000}, 
+                            headers=self.headers)
+    
+    @task(20)
+    def HeavyWeightPost(self):
+        self.client.request("POST","/ok/0/0", 
+                            data={"data":"0"*5000000}, 
+                            headers=self.headers)
+   
+    @task(10)
+    def Error404(self):
+      self.client.request("GET","/error/404", 
+                        headers=self.headers)
+   
+    @task(10)
+    def Error403(self):
+      self.client.request("GET","/error/403", 
+                        headers=self.headers)
 
-  @task(10)
-  def HighLatencyMediumWeight(self):
-      self.client.get("/ok/5000/50000")
-      
-  @task(20)
-  def NoLatencyHeavyWeight(self):
-      self.client.get("/ok/0/5000000")
-
-  @task(20)
-  def MediumLatencyHeavyWeight(self):
-      self.client.get("/ok/500/5000000")
-
-  @task(10)
-  def HighLatencyHeavyWeight(self):
-      self.client.get("/ok/5000/5000000")
-
-  @task(100)
-  def LightWeightPost(self):
-      self.client.post("/ok/0/0",{"data":"0"*500})
-
-  @task(100)
-  def MediumWeightPost(self):
-      self.client.post("/ok/0/0",{"data":"0"*50000})
-
-  @task(20)
-  def HeavyWeightPost(self):
-      self.client.post("/ok/0/0",{"data":"0"*5000000})
-
-  @task(10)
-  def Error404(self):
-    self.client.get("/error/404")
-
-  @task(10)
-  def Error403(self):
-    self.client.get("/error/403")
-  
-  @task(10)
-  def Error500(self):
-    self.client.get("/error/500")
-
-  @task(10)
-  def Error503(self):
-    self.client.get("/error/503")
+    @task(10)
+    def Error500(self):
+      self.client.request("GET","/error/500", 
+                        headers=self.headers)
+    
+    @task(10)
+    def Error503(self):
+      self.client.request("GET","/error/503", 
+                        headers=self.headers)
 
 class TrafficMirrorUser(HttpLocust):
-  task_set = MyTaskSet
-  wait_time = between(5, 15)
+    task_set = MyTaskSet    
+    wait_time = between(5, 15)
